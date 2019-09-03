6752a405a44c36a7db84ce1b5400ab24ac07aaf5
Saugat Acharya <mesaugat@gmail.com>
2019-09-04 00:31:29 +0545

    Start a goroutine to check and tweet progress of bikram sambat

--- main.go
+++ main.go
@@ -1,9 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"time"
+)
+
+// checkBSProgress checks for total BS progress every second. Whenever
+// the initial progress and the running progress has a difference of
+// one, it tweets out a progress string.
+func checkBSProgress() {
+	currentProgress := TotalProgress()
+	ticker := time.NewTicker(time.Second * 1)
+
+	for range ticker.C {
+		runningProgress := TotalProgress()
+		diff := int(runningProgress) - int(currentProgress)
+
+		if diff >= 1 {
+			currentProgress = runningProgress
+			progress := ProgressString(int(currentProgress))
+
+			Tweet(progress)
+		}
+	}
+}
 
 func main() {
-	progress := BSProgressString()
+	go checkBSProgress()
 
-	fmt.Print(progress)
+	select {}
 }
