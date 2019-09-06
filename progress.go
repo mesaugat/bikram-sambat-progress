5ffe7cf26cb81741b99492df1bbe9f80cf24dc0f
Saugat Acharya <mesaugat@gmail.com>
2019-09-06 09:47:59 +0545

    Progress bar with nepali numerals
    Convert arabic numerals to nepali numeral

--- progress.go
+++ progress.go
@@ -2,14 +2,28 @@ package main
 
 import (
 	"log"
+	"strconv"
 	"time"
 
 	"github.com/srishanbhattarai/nepcal/dateconv"
 	"github.com/tj/go-progress"
 )
 
+var nepaliNumerals = map[string]string{
+	"0": "०",
+	"1": "१",
+	"2": "२",
+	"3": "३",
+	"4": "४",
+	"5": "५",
+	"6": "६",
+	"7": "७",
+	"8": "८",
+	"9": "९",
+}
+
 // ProgressString returns a progress bar made of Unicode symbols
-// for the progress value
+// for the progress value.
 func ProgressString(value int) string {
 	b := progress.NewInt(100)
 
@@ -17,13 +31,16 @@ func ProgressString(value int) string {
 	b.StartDelimiter = ""
 	b.EndDelimiter = ""
 
-	b.Template(`{{.Bar}} {{.Percent}}%`)
+	text := convToNepaliNumeral(value)
+
+	b.Template(`{{.Bar}} {{.Text}}%`)
 	b.ValueInt(value)
+	b.Text(text)
 
 	return b.String()
 }
 
-// TotalProgress calculates the floating progress of BS year up until now
+// TotalProgress calculates the floating progress of BS year up until now.
 func TotalProgress() float32 {
 	loc, _ := time.LoadLocation("Asia/Kathmandu")
 	t := time.Now().In(loc)
@@ -39,7 +56,7 @@ func TotalProgress() float32 {
 }
 
 // totalSecondsSpanned calculates the number of seconds that has been
-// spanned up until now
+// spanned up until now.
 func totalSecondsSpanned(t time.Time) int {
 	hour := t.Hour()
 	min := t.Minute()
@@ -51,10 +68,20 @@ func totalSecondsSpanned(t time.Time) int {
 }
 
 // totalSecondsInYear calculates the total number of seconds in the
-// current BS year
+// current BS year.
 func totalSecondsInYear(t time.Time) int {
 	year, _, _ := dateconv.ToBS(t).Date()
 	totalDays, _ := dateconv.TotalDaysInBSYear(year)
 
 	return totalDays * 24 * 60 * 60
+}
+
+// convToNepaliNumeral converts arabic numeral to it's nepali counterpart.
+func convToNepaliNumeral(value int) string {
+	text := ""
+	for _, c := range strconv.Itoa(value) {
+		text = text + nepaliNumerals[string(c)]
+	}
+
+	return text
 }
