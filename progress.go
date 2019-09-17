9312c071b2a0c0c26ead637cb4bb90e8de5ac08b
Saugat Acharya <mesaugat@gmail.com>
2019-09-18 01:42:21 +0545

    Add condition to increase day count for time less than 5:45 NPT

--- progress.go
+++ progress.go
@@ -62,9 +62,29 @@ func totalSecondsSpanned(t time.Time) int {
 	min := t.Minute()
 	sec := t.Second()
 
+	// Because dateconv.TotalDaysSpanned() is based on UTC (if system
+	// time is in UTC), we have a skewed result when calculating
+	// total seconds spanned. For example 2019/09/10 18:15:00 UTC
+	// is 2019/09/11 00:00:00 NST. On the next second, the total
+	// seconds spanned drops to a lower value as total days
+	// spanned still remains the same because of the
+	// +05:45 difference.
 	days, _ := dateconv.TotalDaysSpanned()
 
-	return (days-1)*24*60*60 + hour*60*60 + min*60 + sec
+	// The workaround is to increase the day count by 1
+	// until UTC time reaches a new day (i.e. +5:45)
+	// so that total seconds spanned doesn't drop off
+	// inconsistently.
+	skew := 0
+
+	// If current time is less than 5:45
+	if hour*60*60+min*60 < 5*60*60+45*60 {
+		skew = 24 * 60 * 60
+	}
+
+	totalSeconds := (days-1)*24*60*60 + hour*60*60 + min*60 + sec + skew
+
+	return totalSeconds
 }
 
 // totalSecondsInYear calculates the total number of seconds in the
@@ -72,8 +92,9 @@ func totalSecondsSpanned(t time.Time) int {
 func totalSecondsInYear(t time.Time) int {
 	year, _, _ := dateconv.ToBS(t).Date()
 	totalDays, _ := dateconv.TotalDaysInBSYear(year)
+	totalSeconds := totalDays * 24 * 60 * 60
 
-	return totalDays * 24 * 60 * 60
+	return totalSeconds
 }
 
 // convToNepaliNumeral converts arabic numeral to it's nepali counterpart.
